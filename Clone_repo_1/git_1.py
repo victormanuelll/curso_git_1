8b7ae04d036794d7e3780f145c30a88ee3e4d1c2
Victor <MUNOZVICTOR@edu.unisinos.br>
2025-06-02 21:00:14 -0300

    renomeando

--- Clone_repo_1/git_1.py
+++
@@ -1,6 +0,0 @@
-print ("Hello, World!")
-
-print ("This is a test script for Git.")
-
-print ("This script is used to demonstrate how to clone a repository using Git.")
-print ("This is the first version of the script.")
